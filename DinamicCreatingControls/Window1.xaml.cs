b8b863dc10a890f3a3221f2d8fe74a873d37a602
Vadim Pashaev <vmp@live.ru>
2012-11-04 18:43:01 +0400

    Added dynamic creating controls example.

---
+++ DinamicCreatingControls/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 04.11.2012
+ * Time: 18:20
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DinamicCreatingControls
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+			
+			CreateButtonArray(this.wrpPanel1);
+		}
+		
+		/// <summary>
+		/// Dynamic creating components in WrapPanel.
+		/// </summary>
+		/// <param name="pnl"></param>
+		public void CreateButtonArray(WrapPanel pnl)
+		{
+			Button btn = null;
+			
+			for (int i=0; i<10; i++)
+			{
+				btn = new Button();
+				btn.Content = "button" + i.ToString();
+				btn.Width = 100;
+				btn.Height = 23;
+				btn.Margin = new Thickness(5,5,5,5);
+				pnl.Children.Add(btn);
+			}
+		}
+	}
+}
