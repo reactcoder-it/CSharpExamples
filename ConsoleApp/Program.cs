caabc557f30fc1d9773657f37ad72edb4105f946
Vadim Pashaev <vmp@live.ru>
2012-11-27 23:12:22 +0400

    Добавлен шаблон консольного проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 27.11.2012
+ * Time: 23:11
+ */
+using System;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
