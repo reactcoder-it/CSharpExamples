c95c5396e46560b24180e80ba6e97b9c0c679678
vmp <vmp@live.ru>
2012-11-28 10:49:54 +0300

    Добавлен пример для работы с параллельными потоками.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,8 @@
  * Time: 23:11
  */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -12,9 +14,41 @@ namespace ConsoleApp
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Action<object> action = (object obj) =>
+			{
+				Console.WriteLine("Task={0}, obj={1}, Thread={2}", 
+				                  Task.CurrentId, obj.ToString(), 
+				                  Thread.CurrentThread.ManagedThreadId);
+			};
 			
-			// TODO: Implement Functionality Here
+			// Создаем задачу
+			Task t1 = new Task(action, "alpha");
+			
+			// Создаем стартовую задачу
+			Task t2 = Task.Factory.StartNew(action, "beta");
+			
+			// Останавливаем основной поток для демонстрации, что задача t2 запущена
+			t2.Wait();
+			
+			// Запускаем задачу t1
+			t1.Start();
+			
+			Console.WriteLine("t1 has been launched! (MainThread={0})", Thread.CurrentThread.ManagedThreadId);
+			
+			// Ждем когда задача завершится. Опционально вы можете использовать
+			// Timeout или Cancellation token для уменьшения ситуаций, когда
+			// задача может выполняться очень долго.
+			t1.Wait();
+			
+			// Создаем новую задачу
+			Task t3 = new Task(action, "gamma");
+			
+			// Запускаем ее синхронно
+			t3.RunSynchronously();
+			
+			// Хотя задача была выполнена синхронно, хорошо будет ожидание ее,
+			// за исключение потенциально брошенной задачи.
+			t3.Wait();
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
