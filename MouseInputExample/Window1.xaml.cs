f517c5647aac9fb111ae2846aa3406bcf0323086
Vadim Pashaev <vmp@live.ru>
2012-12-13 21:17:47 +0400

    Добавлен пример с обработкой события MouseMove.

---
+++ MouseInputExample/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 13.12.2012
+ * Time: 21:08
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MouseInputExample
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		
+		void Canvas_MouseMove(object sender, MouseEventArgs e)
+		{
+			Point pt = e.GetPosition(this);
+			this.txtInfo.Text = String.Format("You are at ({0},{1}) in window coordinates", pt.X, pt.Y);
+		}
+	}
+}
