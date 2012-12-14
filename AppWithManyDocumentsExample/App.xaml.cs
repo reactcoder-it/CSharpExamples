e150486f3df1d4224029722490740f014ef44047
Vadim Pashaev <vmp@live.ru>
2012-12-14 20:28:13 +0400

    Добавлен пример для работы с несколькими окнами через класс Application.

---
+++ AppWithManyDocumentsExample/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Data;
+using System.Xml;
+using System.Configuration;
+
+namespace AppWithManyDocumentsExample
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+		private List<Window> _documents = new List<Window>();
+		public List<Window> Documents {
+			get { return this._documents; }
+			set { this._documents = value; }
+		}
+	}
+}
