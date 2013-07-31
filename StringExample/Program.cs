d6a6a3d652c8cb85c6a2f18dab35c245c1ac010d
Vadim Pashaev <vmp@live.ru>
2013-07-31 21:57:54 +0400

    Добавлен пример с типом String который показывает, что String - это ссылочный тип и размещается он в куче, а не в стеке. Но его поведение немного отличается от ссылочных типов.

---
+++ StringExample/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 31.07.2013
+ * Time: 21:52
+ */
+using System;
+
+namespace StringExample
+{
+	class Program
+	{
+		public static int Main()
+		{
+			Console.WriteLine("Hello World!");
+			
+			String s1 = "Строка";
+			String s2 = s1;
+			Console.WriteLine("s1 равно " + s1);
+			Console.WriteLine("s2 равно " + s2);
+			
+			s1 = "Другая строка";
+			Console.WriteLine("s1 теперь равно " + s1);
+			Console.WriteLine("s2 теперь равно " + s2);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			return 0;
+		}
+	}
+}
