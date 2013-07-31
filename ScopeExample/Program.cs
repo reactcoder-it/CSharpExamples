7dfb4ee1b5b23b5285514c5fe854a0536b6e40c9
Vadim Pashaev <vmp@live.ru>
2013-07-31 21:41:11 +0400

    Добавлен пример с областью видимости переменных.

---
+++ ScopeExample/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 31.07.2013
+ * Time: 21:37
+ */
+using System;
+
+namespace ScopeExample
+{
+	class Program
+	{
+		static int j = 20;
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			int j = 30;
+			Console.WriteLine("j = " + j);
+			Console.WriteLine("Program.j = " + Program.j);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
