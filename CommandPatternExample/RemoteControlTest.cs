3350a6b825f53505f81d56b691b490709efc2690
vmp <vmp@live.ru>
2013-08-01 13:15:19 +0300

    Добавлены коментарии, поясняющие работу алгоритма.

--- CommandPatternExample/RemoteControlTest.cs
+++ CommandPatternExample/RemoteControlTest.cs
@@ -14,13 +14,19 @@ namespace CommandPatternExample
 		{
 			Console.WriteLine("Hello World!");
 			
+			// Пульт
 			SimpleRemoteControl remote = new SimpleRemoteControl();
+			
+			// Объекты управления: свет и дверь гаража.
 			Light light = new Light("Kitchen");
 			GarageDoor garageDoor = new GarageDoor("");
 			
+			// Команды пульта
 			LightOnCommand lightOn = new LightOnCommand(light);
 			GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
 			
+			// Поскольку в пульте пока один слот,
+			// то меняем команды динамически.
 			remote.setCommand(lightOn);
 			Console.WriteLine(remote.buttonWasPressed());
 			remote.setCommand(garageOpen);
