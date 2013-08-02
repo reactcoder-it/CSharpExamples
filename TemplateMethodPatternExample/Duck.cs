c802c3febe2b9ee774ff72ece7f77264302e410a
vmp <vmp@live.ru>
2013-08-02 12:24:52 +0300

    Добавлен пример с применением шаблонного метода.

---
+++ TemplateMethodPatternExample/Duck.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 02.08.2013
+ * Time: 11:42
+ */
+using System;
+
+namespace TemplateMethodPatternExample
+{
+	/// <summary>
+	/// Description of Duck.
+	/// </summary>
+	public class Duck : IComparable
+	{
+		String name;
+		int weight;
+		
+		public Duck(String name, int weight)
+		{
+			this.name = name;
+			this.weight = weight;
+		}
+		
+		public override string ToString()
+		{
+			return name + " weights " + weight;
+		}
+		
+		public int CompareTo(object obj)
+		{
+			Duck otherDuck = (Duck)obj;
+			if (this.weight < otherDuck.weight) {
+				return -1;
+			} else if (this.weight == otherDuck.weight) {
+				return 0;
+			} else {
+				return 1;
+			}
+		}
+	}
+}
---
+++ TemplateMethodPatternExample/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 02.08.2013
+ * Time: 11:42
+ */
+using System;
+
+namespace TemplateMethodPatternExample
+{
+	class DuckSortTestDrive
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Duck[] ducks = {
+				new Duck("Daffy", 8),
+				new Duck("Dewey", 2),
+				new Duck("Howard", 7),
+				new Duck("Louie", 2),
+				new Duck("Donald", 10),
+				new Duck("Huey", 2)
+			};
+			
+			Console.WriteLine("Before sorting:");
+			Display(ducks);
+			
+			Array.Sort(ducks);
+			
+			Console.WriteLine("\nAfter sorting:");
+			Display(ducks);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		public static void Display(Duck[] ducks)
+		{
+			for (int i=0; i<ducks.Length; i++) {
+				Console.WriteLine(ducks[i]);
+			}
+		}
+	}
+}
