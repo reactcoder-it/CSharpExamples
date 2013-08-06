a2cc341f97955e3ccdaa34808be5f22bddb0050c
vmp <vmp@live.ru>
2013-08-06 13:10:32 +0300

    Добавлен счетчик кряков реализованного с помощью паттерна Декоратор.

--- MVCDuckExample/DuckSimulator.cs
+++ MVCDuckExample/DuckSimulator.cs
@@ -14,10 +14,10 @@ namespace MVCDuckExample
 	public class DuckSimulator
 	{
 		public void Simulate() {
-			IQuackable mallardDuck = new MallardDuck();
-			IQuackable redheadDuck = new RedheadDuck();
-			IQuackable duckCall = new DuckCall();
-			IQuackable rubberDuck = new RubberDuck();
+			IQuackable mallardDuck = new QuackCounter(new MallardDuck());
+			IQuackable redheadDuck = new QuackCounter(new RedheadDuck());
+			IQuackable duckCall = new QuackCounter(new DuckCall());
+			IQuackable rubberDuck = new QuackCounter(new RubberDuck());
 			IQuackable gooseDuck = new GooseAdapter(new Goose());
 			
 			Console.WriteLine("\nDuck Simulator: With Goose Adapter");
@@ -27,6 +27,8 @@ namespace MVCDuckExample
 			Simulate(duckCall);
 			Simulate(rubberDuck);
 			Simulate(gooseDuck);
+			
+			Console.WriteLine("The ducks quacked " + QuackCounter.GetQuacks() + " times");
 		}
 		
 		public void Simulate(IQuackable duck) {
---
+++ MVCDuckExample/QuackCounter.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 06.08.2013
+ * Time: 13:00
+ */
+using System;
+
+namespace MVCDuckExample
+{
+	/// <summary>
+	/// Description of QuackCounter.
+	/// </summary>
+	public class QuackCounter : IQuackable
+	{
+		IQuackable duck;
+		static int numberOfQuacks;
+		
+		public QuackCounter(IQuackable duck) {
+			this.duck = duck;
+		}
+		
+		public void Quack() {
+			duck.Quack();
+			numberOfQuacks++;
+		}
+		
+		public static int GetQuacks() {
+			return numberOfQuacks;
+		}
+	}
+}
