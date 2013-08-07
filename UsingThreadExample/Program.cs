272c6e2c97764a0e400c9e36da2b1ed8d8bdeb9d
vmp <vmp@live.ru>
2013-08-07 11:07:55 +0300

    Добавлено решение с примерами из книги Kort W. - Exam Ref 70-483. Programming in C#.

---
+++ UsingThreadExample/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 07.08.2013
+ * Time: 10:49
+ */
+using System;
+using System.Threading;
+
+namespace UsingThreadExample
+{
+	public static class Program
+	{
+		public static void ThreadMethod() {
+			for (int i=0; i<10; i++) {
+				Console.WriteLine("ThreadProc: {0}", i);
+				Thread.Sleep(0);
+			}
+		}
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Thread t = new Thread(new ThreadStart(ThreadMethod));
+			t.Start();
+			
+			for (int i=0; i<4; i++) {
+				Console.WriteLine("MainThread: Do Some Work.");
+				Thread.Sleep(0);
+			}
+			
+			t.Join();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
