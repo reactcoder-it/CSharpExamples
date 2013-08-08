b0986fcc5aeb4617bc1171261e2367a7af04e387
vmp <vmp@live.ru>
2013-08-08 13:30:52 +0300

    Добавлен пример с использованием асинхронного метода при нажатии на кнопку. Данный пример вызовет ошибку ( Output.Content = content; ), поскольку ConfigureAwait(false).

---
+++ CofigureAwaitUsing/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 08.08.2013
+ * Time: 12:35
+ */
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Threading.Tasks;
+
+namespace CofigureAwaitUsing
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			HttpClient httpClient = new HttpClient();
+			
+			string content = await httpClient
+				.GetStringAsync("http://www.microsoft.com")
+				.ConfigureAwait(false);
+			
+			label.Content = content;
+		}
+	}
+}
