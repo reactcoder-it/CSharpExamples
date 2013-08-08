0bc3d96c1fe1fca26ccf20f66fc12dd12c5b0437
vmp <vmp@live.ru>
2013-08-08 10:28:17 +0300

    В данном примере для остановки цикла используется объект ParallelLoopState.

--- UsingThreadExample/Program.cs
+++ UsingThreadExample/Program.cs
@@ -17,15 +17,14 @@ namespace UsingThreadExample
 		{
 			Console.WriteLine("Hello World!");
 			
-			Parallel.For(0, 10, i =>
+			ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
 			{
-				Thread.Sleep(1000);
-			});
-			
-			var numbers = Enumerable.Range(0, 10);
-			Parallel.ForEach(numbers, i =>
-			{
-				Thread.Sleep(1000);
+				if (i == 500)
+				{
+					Console.WriteLine("Breaking loop");
+					loopState.Break();
+				}
+				return;
 			});
 			
 			Console.Write("Press any key to continue . . . ");
