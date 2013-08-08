308b166451ad2edf57db62d97d1e06fe71b78ed4
vmp <vmp@live.ru>
2013-08-08 14:15:04 +0300

    Добавлен пример с применением технологии PLINQ.

---
+++ UsingPLINQExample/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 08/08/2013
+ * Time: 14:01
+ */
+using System;
+using System.Linq;
+
+namespace UsingPLINQExample
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			var numbers = Enumerable.Range(0, 10);
+			var parallelResult = numbers.AsParallel()
+				.Where(i => i % 2 == 0)
+				.ToArray();
+			
+			foreach (int i in parallelResult)
+			{
+				Console.WriteLine(i);
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
