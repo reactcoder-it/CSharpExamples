f15686ea508f6b385f87fa175e415bc252ed877a
vmp <vmp@live.ru>
2013-08-09 08:15:49 +0300

    При возникновении исключений в параллельной обработке все они собираются объектом AggregateException.

--- UsingPLINQExample/Program.cs
+++ UsingPLINQExample/Program.cs
@@ -16,14 +16,29 @@ namespace UsingPLINQExample
 			Console.WriteLine("Hello World!");
 			
 			var numbers = Enumerable.Range(0, 20);
-			var parallelResult = numbers
-				.AsParallel()
-				.Where(i => i % 2 == 0);
 			
-			parallelResult.ForAll(e => Console.WriteLine(e));
+			try
+			{
+				var parallelResult = numbers
+					.AsParallel()
+					.Where(i => IsEven(i));
+			
+				parallelResult.ForAll(e => Console.WriteLine(e));
+			}
+			catch (AggregateException e)
+			{
+				Console.WriteLine("There where {0} exceptions", e.InnerExceptions.Count);
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		public static bool IsEven(int i)
+		{
+			if (i % 10 == 0) throw new ArgumentException("i");
+			
+			return i % 2 == 0;
 		}
 	}
 }
