a7b5e2d3feed1cbb9944a1f657f76a6a8b69da8d
vmp <vmp@live.ru>
2013-08-09 13:33:22 +0300

    Добавлен пример с использованием класса Interlocked.

---
+++ SynchronizingResourcesExample/UsingInterlocked.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 09.08.2013
+ * Time: 13:29
+ */
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizingResourcesExample
+{
+	/// <summary>
+	/// Синхронизацию возможно сделать и другим способом.
+	/// Использовать класс Interlocked, которые представляет
+	/// атомарные операции, которые не могут быть разделены
+	/// на несколько операций.
+	/// </summary>
+	public class UsingInterlocked
+	{
+		static void Main()
+		{
+			int n = 0;
+			
+			var up = Task.Run(() =>
+			{
+				for (int i = 0; i < 1000000; i++)
+				{
+					Interlocked.Increment(ref n);
+				}
+			});
+			
+			for (int i = 0; i < 1000000; i++)
+			{
+				Interlocked.Decrement(ref n);
+			}
+			
+			up.Wait();
+			Console.WriteLine(n);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
