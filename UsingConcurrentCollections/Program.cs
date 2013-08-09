e4e67bec0d645a6ac12494455f5c5de8ec57ecb4
vmp <vmp@live.ru>
2013-08-09 08:30:05 +0300

    Добавлен пример использующий BlockingCollection. Одна задача добавляет данные в коллекцию, а вторая - удалает их из коллекции.

---
+++ UsingConcurrentCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 09.08.2013
+ * Time: 8:23
+ */
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace UsingConcurrentCollections
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			BlockingCollection<String> col = new BlockingCollection<string>();
+			Task read = Task.Run(() =>
+			{
+				while (true)
+				{
+					Console.WriteLine(col.Take());
+				}
+			});
+			
+			Task write = Task.Run(() =>
+			{
+				while (true)
+				{
+					String s = Console.ReadLine();
+					if (String.IsNullOrWhiteSpace(s)) break;
+					col.Add(s);
+				}
+			});
+			
+			write.Wait();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
