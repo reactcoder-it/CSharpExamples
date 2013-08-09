72a64e0a4e0fcc90a7cef5b34e61c1a880bb98fd
Vadim Pashaev <vmp@live.ru>
2013-08-09 20:00:48 +0400

    В данном примере показывается как использовать условный оператор.

--- ImplementingProgramFlow/Program.cs
+++ ImplementingProgramFlow/Program.cs
@@ -23,8 +23,20 @@ namespace ImplementingProgramFlow
 			int d = c ?? z ?? -1;
 			Console.WriteLine(d);
 			
+			Console.WriteLine(GetValue(true));
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		private static int GetValue(bool p)
+		{
+//			if (p)
+//				return 1;
+//			else
+//				return 0;
+			
+			return p ? 1 : 0;
 		}
 	}
 }
