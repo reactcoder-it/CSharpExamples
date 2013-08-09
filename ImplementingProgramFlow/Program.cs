b75765d2fdb485f45bef167973c44ee7e6a0cf62
Vadim Pashaev <vmp@live.ru>
2013-08-09 19:49:41 +0400

    Добавлен пример использования оператора null-coalescing.

---
+++ ImplementingProgramFlow/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 09.08.2013
+ * Time: 19:45
+ */
+using System;
+
+namespace ImplementingProgramFlow
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			int? x = null;
+			int y = x ?? -1;
+			Console.WriteLine(y);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
