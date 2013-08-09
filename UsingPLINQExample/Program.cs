7b1d2bd397ccf2ae25337a1a91dc2c0b6de90c33
vmp <vmp@live.ru>
2013-08-09 08:01:26 +0300

    В данном примере перед выводом параллельная очередь преобразуется в последовательную.

--- UsingPLINQExample/Program.cs
+++ UsingPLINQExample/Program.cs
@@ -15,12 +15,14 @@ namespace UsingPLINQExample
 		{
 			Console.WriteLine("Hello World!");
 			
-			var numbers = Enumerable.Range(0, 10);
-			var parallelResult = numbers.AsParallel().AsOrdered()
+			var numbers = Enumerable.Range(0, 20);
+			var parallelResult = numbers
+				.AsParallel()
+				.AsOrdered()
 				.Where(i => i % 2 == 0)
-				.ToArray();
+				.AsSequential();
 			
-			foreach (int i in parallelResult)
+			foreach (int i in parallelResult.Take(5))
 			{
 				Console.WriteLine(i);
 			}
