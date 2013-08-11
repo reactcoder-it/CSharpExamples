2f619c43ef7ecf6e05a134a14ff4e527790f1560
Vadim Pashaev <vmp@live.ru>
2013-08-11 17:59:00 +0400

    Добавлен пример использующий Environment.FailFast для немедленного завершения приложения и записи информации в Windows event log.

---
+++ ExceptionHandling/UsingEnvironmentFailFast.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 11.08.2013
+ * Time: 17:55
+ */
+using System;
+
+namespace ExceptionHandling
+{
+	/// <summary>
+	/// Description of UsingEnvironmentFailFast.
+	/// </summary>
+	public class UsingEnvironmentFailFast
+	{
+		public static void Main(string[] args)
+		{
+			String s = Console.ReadLine();
+			//if (String.IsNullOrWhiteSpace(s)) break;
+				
+			try
+			{
+				int i = int.Parse(s);
+				if (i == 42) Environment.FailFast("Special number entered");
+			}
+			catch (ArgumentNullException)
+			{
+				Console.WriteLine("You need to enter a value");
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("{0} is not valid number. Please try again", s);
+			}
+			finally
+			{
+				Console.WriteLine("Program complete.");
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
