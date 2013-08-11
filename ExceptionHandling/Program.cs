0c1d471143cff5d2cdc99c56bc56694762cd187d
Vadim Pashaev <vmp@live.ru>
2013-08-11 17:23:23 +0400

    Добавлен пример обработки исключения.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 11.08.2013
+ * Time: 17:13
+ */
+using System;
+
+namespace ExceptionHandling
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			while (true)
+			{
+				String s = Console.ReadLine();
+				if (String.IsNullOrWhiteSpace(s)) break;
+				
+				try
+				{
+					int i = int.Parse(s);
+					break;
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("{0} is not valid number. Please try again", s);
+				}
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
