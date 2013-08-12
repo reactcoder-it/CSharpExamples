a0594c5ecd9d41aa6735a7c34dcd036073ccfcd8
vmp <vmp@live.ru>
2013-08-12 09:16:38 +0300

    Добавлен пользовательский класс исключения.

---
+++ ExceptionHandling/OrderProcessException.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 12.08.2013
+ * Time: 8:48
+ */
+using System;
+using System.Runtime.Serialization;
+
+namespace ExceptionHandling
+{
+	/// <summary>
+	/// Description of OrderProcessException.
+	/// </summary>
+	[Serializable]
+	public class OrderProcessException : Exception, ISerializable
+	{
+		public int OrderId { get; set; }
+		
+		public OrderProcessException(int orderId)
+		{
+			this.OrderId = orderId;
+			this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+		}
+		
+		public OrderProcessException(int orderId, string message)
+			: base(message)
+		{
+			this.OrderId = orderId;
+			this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+		}
+		
+		public OrderProcessException(int orderId, string message, Exception innerException)
+			: base(message, innerException)
+		{
+			this.OrderId = orderId;
+			this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+		}
+		
+		protected OrderProcessException(SerializationInfo info,StreamingContext context)
+		{
+			this.OrderId = (int)info.GetValue("OrderId", typeof(int));
+		}
+		
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("OrderId", OrderId, typeof(int));
+		}
+	}
+}
