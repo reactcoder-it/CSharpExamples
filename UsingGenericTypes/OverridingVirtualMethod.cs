fb5b4410c74ad860c5835086ed3afed5dfc362e2
vmp <vmp@live.ru>
2013-08-13 16:06:06 +0300

    Добавлен пример перегрузки абстрактных методов и защита от перегрузки sealed.

---
+++ UsingGenericTypes/OverridingVirtualMethod.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 13.08.2013
+ * Time: 16:00
+ */
+using System;
+
+namespace UsingGenericTypes
+{
+	class Base
+	{
+		public virtual int MyMethod()
+		{
+			return 42;
+		}
+	}
+	
+	class Derived : Base
+	{
+		public sealed override int MyMethod()
+		{
+			return base.MyMethod() * 2;
+		}
+	}
+	
+	class Derived2 : Derived
+	{
+		// This line would give a compile error
+		//public override int MyMethod() { return 1; }
+	}
+}
