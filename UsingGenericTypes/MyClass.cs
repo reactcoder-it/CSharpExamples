807cf2bb9fe004f10972708b786639bf88f3ad90
vmp <vmp@live.ru>
2013-08-13 14:22:08 +0300

    Добавлен пример с обобщениями.

---
+++ UsingGenericTypes/MyClass.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 13.08.2013
+ * Time: 14:14
+ */
+using System;
+
+namespace UsingGenericTypes
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass<T> where T : class, new()
+	{
+		T MyProperty { get; set; }
+		
+		public MyClass()
+		{
+			MyProperty = new T();
+		}
+		
+		public void MyGenericMethod<T>()
+		{
+			T defaultValue = default(T);
+		}
+	}
+}
---
+++ UsingGenericTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 13.08.2013
+ * Time: 14:13
+ */
+using System;
+
+namespace UsingGenericTypes
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
