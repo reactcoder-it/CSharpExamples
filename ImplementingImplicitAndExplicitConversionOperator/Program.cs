9f50ea59c089cd6ad0fd14a102e98343bcd201b5
Vadim Pashaev <vmp@live.ru>
2013-08-13 20:05:21 +0400

    Added example with user-defined conversions of types.

---
+++ ImplementingImplicitAndExplicitConversionOperator/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 13.08.2013
+ * Time: 19:59
+ */
+using System;
+
+namespace ImplementingImplicitAndExplicitConversionOperator
+{
+	class Money
+	{
+		public decimal Amount { get; set; }
+		
+		public Money(decimal amount)
+		{
+			Amount = amount;
+		}
+		
+		public static implicit operator decimal(Money money)
+		{
+			return money.Amount;
+		}
+		
+		public static explicit operator int(Money money)
+		{
+			return (int)money.Amount;
+		}
+	}
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Money m = new Money(42.42M);
+			decimal amount = m;
+			int truncatedAmount = (int)m;
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
