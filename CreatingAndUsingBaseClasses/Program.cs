829042a45db2097d087eaecff67833f52a3fb4a2
vmp <vmp@live.ru>
2013-08-14 11:39:49 +0300

    Добавлен пример использования скрытия метода базового класса и переопределения метода базового класса (результаты совсем разные).

---
+++ CreatingAndUsingBaseClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 14.08.2013
+ * Time: 11:32
+ */
+using System;
+
+namespace CreatingAndUsingBaseClasses
+{
+	class Base
+	{
+//		public void Execute()
+//		{
+//			Console.WriteLine("Base.Execute");
+//		}
+		
+		public virtual void Execute()
+		{
+			Console.WriteLine("Base.Execute");
+		}
+	}
+	
+	class Derived : Base
+	{
+//		public new void Execute()
+//		{
+//			Console.WriteLine("Derived.Execute");
+//		}
+		
+		public override void Execute()
+		{
+			//base.Execute();
+			Console.WriteLine("Derived.Execute");
+		}
+	}
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Base b = new Base();
+			b.Execute();
+			b = new Derived();
+			b.Execute();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
