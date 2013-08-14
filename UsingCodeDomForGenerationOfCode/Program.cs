64f40854dbd49e6058f7b033344d6f9072441783
Vadim Pashaev <vmp@live.ru>
2013-08-14 20:06:09 +0400

    Added example with CodeDom code generation.

---
+++ UsingCodeDomForGenerationOfCode/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 14.08.2013
+ * Time: 19:06
+ */
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using Microsoft.CSharp;
+
+namespace UsingCodeDomForGenerationOfCode
+{
+	class Program
+	{
+		static CodeCompileUnit GetCode()
+		{
+			CodeCompileUnit compileUnit = new CodeCompileUnit();
+			CodeNamespace myNamespace = new CodeNamespace("MyNamespace");
+			myNamespace.Imports.Add(new CodeNamespaceImport("System"));
+			CodeTypeDeclaration myClass = new CodeTypeDeclaration("MyClass");
+			CodeEntryPointMethod start = new CodeEntryPointMethod();
+			CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
+				new CodeTypeReferenceExpression("Console"),
+				"WriteLine", new CodePrimitiveExpression("Hello World!"));
+			compileUnit.Namespaces.Add(myNamespace);
+			myNamespace.Types.Add(myClass);
+			myClass.Members.Add(start);
+			start.Statements.Add(cs1);
+			return compileUnit;
+		}
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			CSharpCodeProvider provider = new CSharpCodeProvider();
+			
+			using (StreamWriter sw = new StreamWriter("HelloWorld.cs", false))
+			{
+				IndentedTextWriter tw = new IndentedTextWriter(sw, "   ");
+				provider.GenerateCodeFromCompileUnit(GetCode(), tw, new CodeGeneratorOptions());
+				tw.Close();
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
