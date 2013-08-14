7a2d3d47eace8b8a8fba700205a282fa54438f61
Vadim Pashaev <vmp@live.ru>
2013-08-14 20:22:03 +0400

    Также генерация кода возможна с помощью Expression Trees.

---
+++ UsingCodeDomForGenerationOfCode/CreatingHelloWorldWithExpressionTree.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 14.08.2013
+ * Time: 20:15
+ */
+using System;
+using System.Linq.Expressions;
+
+namespace UsingCodeDomForGenerationOfCode
+{
+	/// <summary>
+	/// Description of CreatingHelloWorldWithExpressionTree.
+	/// </summary>
+	public class CreatingHelloWorldWithExpressionTree
+	{
+		static void Main()
+		{
+			BlockExpression blockExpr = Expression.Block(
+				Expression.Call(
+					null,
+					typeof(Console).GetMethod("Write", new Type[] { typeof(String) }),
+					Expression.Constant("Hello ")
+				),
+				Expression.Call(
+					null,
+					typeof(Console).GetMethod("WriteLine", new Type[] { typeof(String) }),
+					Expression.Constant("World!")
+				)
+			);
+			
+			Expression.Lambda<Action>(blockExpr).Compile()();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
--- UsingCodeDomForGenerationOfCode/Program.cs
+++ UsingCodeDomForGenerationOfCode/Program.cs
@@ -44,6 +44,9 @@ namespace UsingCodeDomForGenerationOfCode
 				tw.Close();
 			}
 			
+			Func<int, int, int> addFunc = (x, y) => x + y;
+			Console.WriteLine(addFunc(2, 3));
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
