a5f7e0ce7962f11d822827b58ee3f6e6290f3f4a
Vadim Pashaev <vmp@live.ru>
2013-08-14 23:10:36 +0400

    Добавлен пример с использованием финализаторов и интерфейса IDisposable.

---
+++ ImplementingIDisposableAndFinalizer/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 14.08.2013
+ * Time: 22:35
+ */
+using System;
+using System.IO;
+
+namespace ImplementingIDisposableAndFinalizer
+{
+	class UnmanagedWrapper : IDisposable
+	{
+		public FileStream Stream { get; private set; }
+		
+		public UnmanagedWrapper() {
+			this.Stream = File.Open("temp.dat", FileMode.Create);
+		}
+		
+		~UnmanagedWrapper() {
+			Dispose(false);
+		}
+		
+		public void Close() {
+			Dispose();
+		}
+		
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		
+		public void Dispose(bool disposing) {
+			if (disposing) {
+				if (Stream != null) {
+					Stream.Close();
+				}
+			}
+		}
+	}
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
