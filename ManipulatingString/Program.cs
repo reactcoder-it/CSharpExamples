cd11baf53695d17cec8640c27d8b67d408614d45
vmp <vmp@live.ru>
2013-08-15 09:07:22 +0300

    Добавлен пример с много-форматным выводом.

--- ManipulatingString/Program.cs
+++ ManipulatingString/Program.cs
@@ -78,8 +78,52 @@ namespace ManipulatingString
 			foreach (string word in "My sentence separated by spaces".Split(' '))
 				Console.WriteLine(word);
 			
+			Person person = new Person("Jane", "Air");
+			Console.WriteLine(person.ToString("FL"));
+			Console.WriteLine(person.ToString("LF"));
+			Console.WriteLine(person.ToString("FSL"));
+			Console.WriteLine(person.ToString("LSF"));
+			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+	}
+	
+	class Person
+	{
+		public Person(string firstName, string lastName)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+		}
+		
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public override string ToString()
+		{
+			return FirstName + LastName;
+		}
+		
+		public string ToString(string format)
+		{
+			if (string.IsNullOrWhiteSpace(format) || format == "G") format = "FL";
+			
+			format = format.Trim().ToUpperInvariant();
+			
+			switch (format)
+			{
+				case "FL":
+					return FirstName + " " + LastName;
+				case "LF":
+					return LastName + " " + FirstName;
+				case "FSL":
+					return FirstName + ", " + LastName;
+				case "LSF":
+					return LastName + ", " + FirstName;
+				default:
+					throw new FormatException(string.Format("The '{0}' format string is not supported.", format));
+			}
 		}
 	}
 }
