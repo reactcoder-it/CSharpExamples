7be3044ee1afebf23faa45505e14012b147c9519
vmp <vmp@live.ru>
2013-08-20 15:05:51 +0300

    Добавлен пример с рефакторингом.

---
+++ GeneratePrimesExample/GeneratePrimesTest.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vadim Pashaev <vmp@live.ru>
+ * Date: 20.08.2013
+ * Time: 14:52
+ */
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace GeneratePrimesExample
+{
+	/// <summary>
+	/// Description of GeneratePrimesTest.
+	/// </summary>
+	[TestFixture]
+	public class GeneratePrimesTest
+	{
+		[Test]
+		public void TestPrimes()
+		{
+			int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+			Assert.AreEqual(nullArray.Length, 0);
+			
+			int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+			Assert.AreEqual(minArray.Length, 1);
+			Assert.AreEqual(minArray[0], 2);
+			
+			int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+			Assert.AreEqual(threeArray.Length, 2);
+			Assert.AreEqual(threeArray[0], 2);
+			Assert.AreEqual(threeArray[1], 3);
+			
+			int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+			Assert.AreEqual(centArray.Length, 25);
+			Assert.AreEqual(centArray[24], 97);
+		}
+	}
+}
