050f3825de35637915fda8fa47e7341312b70ec4
vmp <vmp@live.ru>
2013-08-20 15:15:50 +0300

    Первая версия после рефакторинга.

--- GeneratePrimesExample/Program.cs
+++ GeneratePrimesExample/Program.cs
@@ -11,46 +11,68 @@ namespace GeneratePrimesExample
 {
 	public class GeneratePrimes
 	{
+		private static int s;
+		private static bool[] f;
+		private static int[] primes;
+		
 		public static int[] GeneratePrimeNumbers(int maxValue)
 		{
-			if (maxValue >= 2)
+			if (maxValue < 2)
+				return new int[0];
+			else
 			{
-				int s = maxValue + 1;
-				bool[] f = new bool[s];
-				int i;
-				
-				for (i=0; i<s; i++)
-					f[i] = true;
-				
-				f[0] = f[1] = false;
-				
-				int j;
-				for (i=2; i<Math.Sqrt(s) + 1; i++)
-				{
-					if (f[i])
-					{
-						for (j=2*i; j<s; j+=i)
-							f[j] = false;
-					}
-				}
-				
-				int count = 0;
-				for (i=0; i<s; i++)
-				{
-					if (f[i])
-						count++;
-				}
-				
-				int[] primes = new int[count];
-				for(i=0, j=0; i<s;i++)
-				{
-					if (f[i])
-						primes[j++] = i;
-				}
+				InitializeSieve(maxValue);
+				Sieve();
+				LoadPrimes();
 				return primes;
 			}
-			else
-				return new int[0];
+		}
+		
+		private static void LoadPrimes()
+		{
+			int j;
+			int i;
+			
+			int count = 0;
+			for (i=0; i<s; i++)
+			{
+				if (f[i])
+					count++;
+			}
+			
+			primes = new int[count];
+			for(i=0, j=0; i<s; i++)
+			{
+				if (f[i])
+					primes[j++] = i;
+			}
+		}
+		
+		private static void Sieve()
+		{
+			int i;
+			int j;
+			
+			for (i=2; i<Math.Sqrt(s) + 1; i++)
+			{
+				if (f[i])
+				{
+					for (j=2*i; j<s; j+=i)
+						f[j] = false;
+				}
+			}
+		}
+		
+		private static void InitializeSieve(int maxValue)
+		{
+			s = maxValue + 1;
+			f = new bool[s];
+			int i;
+			
+			for (i=0; i<s; i++)
+				f[i] = true;
+			
+			f[0] = f[1] = false;
 		}
 	}
 	
