80cc6e2a24a221e8559478142505afc5ee67c439
vmp <vmp@live.ru>
2013-08-21 13:56:20 +0300

    Добавлен тест. Работает.

--- BowlingGame/UnitTests/GameTest.cs
+++ BowlingGame/UnitTests/GameTest.cs
@@ -87,5 +87,55 @@ namespace BowlingGame.UnitTests
 			Assert.AreEqual(300, game.Score);
 			Assert.AreEqual(11, game.CurrentFrame);
 		}
+		
+		[Test]
+		public void TestEndOfArray()
+		{
+			for (int i=0; i<9; i++)
+			{
+				game.Add(0);
+				game.Add(0);
+			}
+			game.Add(2);
+			game.Add(8);
+			game.Add(10);
+			Assert.AreEqual(20, game.Score);
+		}
+		
+		[Test]
+		public void TestSampleGame()
+		{
+			game.Add(1);
+			game.Add(4);
+			game.Add(4);
+			game.Add(5);
+			game.Add(6);
+			game.Add(4);
+			game.Add(5);
+			game.Add(5);
+			game.Add(10);
+			game.Add(0);
+			
+			game.Add(1);
+			game.Add(7);
+			game.Add(3);
+			game.Add(6);
+			game.Add(4);
+			game.Add(10);
+			
+			game.Add(2);
+			game.Add(8);
+			game.Add(6);
+			Assert.AreEqual(133, game.Score);
+		}
+		
+		[Test]
+		public void TestHeartBreak()
+		{
+			for (int i=0; i<11; i++) game.Add(10);
+			
+			game.Add(9);
+			Assert.AreEqual(299, game.Score);
+		}
 	}
 }
