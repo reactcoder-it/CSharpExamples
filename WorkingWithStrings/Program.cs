2cb5ec36d35e105583dccb88c1ada7d06decd604
Vadim Pashaev <vmp@live.ru>
2013-09-05 19:43:31 +0400

    Добавлен пример программы с типом char.

---
+++ WorkingWithStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VMP
+ * Date: 05.09.2013
+ * Time: 19:37
+ */
+using System;
+
+namespace WorkingWithStrings
+{
+	class Program
+	{
+		static void WorkingWithChar()
+		{
+			Console.WriteLine("--- Working with char ---");
+			Console.WriteLine(Char.ToUpper('c'));
+			Console.WriteLine(Char.IsWhiteSpace('\t'));
+			Console.WriteLine("-------------------------");
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			WorkingWithChar();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+		
+		
+	}
+}
