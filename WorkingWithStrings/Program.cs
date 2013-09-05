895f887df685d9028640b3e59269f8c41f2e1d1b
Vadim Pashaev <vmp@live.ru>
2013-09-05 20:08:54 +0400

    Добавлен пример со строками.

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -15,19 +15,33 @@ namespace WorkingWithStrings
 			Console.WriteLine("--- Working with char ---");
 			Console.WriteLine(Char.ToUpper('c'));
 			Console.WriteLine(Char.IsWhiteSpace('\t'));
-			Console.WriteLine("-------------------------");
+			Console.WriteLine(Char.GetUnicodeCategory('\t'));
 		}
-
+		
+		static void WorkingWithString()
+		{
+			Console.WriteLine("--- Working with string ---");
+			string s1 = "Hello";
+			string s2 = "First Line\r\nSecond Line";
+			string s3 = @"\\server\fileshare\helloworld.cs";
+			Console.WriteLine(s1);
+			Console.WriteLine(s2);
+			Console.WriteLine(s3);
+			
+			char[] ca = "Hello".ToCharArray();
+			string s = new string(ca);
+			Console.WriteLine(s);
+		}
+		
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
 			
 			WorkingWithChar();
+			WorkingWithString();
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
-		
-		
 	}
 }
